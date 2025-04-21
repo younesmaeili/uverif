f2c466479b3e4686042d9e291efb41768c73a8f5
Younes Esmaeili <155487793+younesmaeili@users.noreply.github.com>
2025-04-21 20:19:08 +0330

    Update dataflow.h

--- src_revised/dataflow.h
+++ src_revised/dataflow.h
@@ -5,7 +5,7 @@
 #include "kernel/yosys.h"
 #include "kernel/sigtools.h"
 #include "kernel/celltypes.h"
-#include "backends/ilang/ilang_backend.h"
+#include "backends/rtlil/rtlil_backend.h"
 #include "kernel/hashlib.h"
 
 #include <string>
